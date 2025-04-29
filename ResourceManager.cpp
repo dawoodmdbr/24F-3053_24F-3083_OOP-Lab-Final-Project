bb9cdda48c141963a84f8ab01018cb57cfae9389
Mian Dawood Bin Rafay <dawoodbinrafaydbr@gmail.com>
2025-04-30 02:27:44 +0500

    Update ResourceManager.cpp

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -5,4 +5,25 @@ ResourceManager::ResourceManager(){
         resources[i] = Resource(static_cast<ResourceType>(i), 100.0);
     }
 }
-void ResourceManager::gather()
+void ResourceManager::gather(ResourceType type, int a) {
+    if (type >= 0 && type < RESOURCE_TYPE_COUNT) {
+        resources[type].gather(a);
+    }
+}
+void ResourceManager::consume(ResourceType type, int a) {
+    if (type >= 0 && type < RESOURCE_TYPE_COUNT) {
+        resources[type].consume(a);
+    }
+}
+void ResourceManager::trade(ResourceType fromType, ResourceType toType, int a) {
+    if (fromType >= 0 && fromType < RESOURCE_TYPE_COUNT && toType >= 0 && toType < RESOURCE_TYPE_COUNT) {
+        resources[fromType].trade(a);
+        resources[toType].gather(a);
+    }
+}
+int ResourceManager::get(ResourceType type) const {
+    if (type >= 0 && type < RESOURCE_TYPE_COUNT) {
+        return resources[type].getAmount();
+    }
+    return 0;
+}
